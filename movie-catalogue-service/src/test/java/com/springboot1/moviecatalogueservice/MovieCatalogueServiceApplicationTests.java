b491d2259e2296cff8f915657f2e63425533cb00
hanmantmca <hanmantmca@gmail.com>
2021-09-14 08:07:47 +0530

    Inital template code Spring Boot Microservices

---
+++ movie-catalogue-service/src/test/java/com/springboot1/moviecatalogueservice/MovieCatalogueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot1.moviecatalogueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogueServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
